a722b749a45368c1158f58d358fe42879e478724
elhedi <hheeddii87@gmail.com>
2024-10-21 22:40:20 +0200

    bulbasaur

---
+++ my-pokedex-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import PokemonCard from "./components/PokemonCard";
+
+function App() {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+  );
+}
+
+export default App;
