0e61c52ad41aab425e9c8dd0eb8b6aea6bbe46c4
elhedi <hheeddii87@gmail.com>
2024-10-22 15:32:36 +0200

    components.2

--- my-pokedex-app/src/components/PokemonCard.tsx
+++ my-pokedex-app/src/components/PokemonCard.tsx
@@ -1,5 +1,25 @@
+const pokemonList = [
+        {
+          name: "bulbasaur",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
+
+
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" /><figcaption>Bulbasaur</figcaption></figure>;
+        const pokemon = pokemonList[0]
+    return (
+        <figure>
+            {
+                pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt="" /> : <p>???</p>
+            }
+            <p>{pokemon.name}</p>
+        </figure>);
+          
   }
-  
+
   export default PokemonCard;
