036c0f4d3800ef57942e7b75bc66e4407176b9c2
elhedi <hheeddii87@gmail.com>
2024-10-25 09:05:13 +0200

    rendering-list.1

--- my-pokedex-app/src/App.tsx
+++ my-pokedex-app/src/App.tsx
@@ -6,56 +6,56 @@ import { useState } from "react";
 
 import NavBar from "./components/NavBar";
 
+const pokemonList = [
+  {
 
+    name: "bulbasaur",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+  },
+
+  {
+
+    name: "charmander",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+
+  },
+
+  {
+
+    name: "squirtle",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+
+  },
+
+  {
+
+    name: "pikachu",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+
+  },
+  {
+    name: "mew",
+  }
+];
 
 
 
 function App() {
   const  [pokemonIndex, setPokemonIndex] = useState(0);
-  const pokemonList = [
-    {
 
-      name: "bulbasaur",
-  
-      imgSrc:
-  
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  
-    },
-  
-    {
-  
-      name: "charmander",
-  
-      imgSrc:
-  
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  
-    },
-  
-    {
-  
-      name: "squirtle",
-  
-      imgSrc:
-  
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-  
-    },
-  
-    {
-  
-      name: "pikachu",
-  
-      imgSrc:
-  
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-  
-    },
-    {
-      name: "mew",
-    }
-];
   return (
     <div>
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
--- my-pokedex-app/src/components/NavBar.tsx
+++ my-pokedex-app/src/components/NavBar.tsx
@@ -9,13 +9,20 @@ interface Pokemon {
     pokemonList: Pokemon[];
   }
 
-  function NavBar ({pokemonIndex,setPokemonIndex}:NavBarProps){
-    const handleClickMore = () => {setPokemonIndex(pokemonIndex + 1)};
-    const handleClickLess = () => {setPokemonIndex(pokemonIndex - 1)};
+  function NavBar ({pokemonIndex,setPokemonIndex,pokemonList}:NavBarProps){
+    const handleClick = (index) => {setPokemonIndex(index)}
     return (
-        <nav>
-        <button type= "button" onClick={handleClickLess}>Precedent</button><button type= "button" onClick={handleClickMore}>Suivant</button>
-        </nav>
+        <>
+          {pokemonList.map((Pokemon,index)=>(
+            <button
+            key={Pokemon.name}
+            onClick={()=>handleClick(index)}
+            >
+              {Pokemon.name}
+            </button>
+          ))}
+
+        </>
     )
   }
 
